21e260bdaae257fe6b88864c899eba380be43911
jkuenzle <j.kuenzle@gmx.net>
2015-03-05 22:07:21 +0100

    Initalfile for SQL - User Table

---
+++ server/sql/createTable.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `user` (
+`id` bigint(20) NOT NULL AUTO_INCREMENT,
+`firstname` varchar(255) NOT NULL,
+`lastname` varchar(255) NOT NULL,
+`phone` varchar(255) NOT NULL,
+`email` varchar(255) NOT NULL,
+PRIMARY KEY (`id`)
+) AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
